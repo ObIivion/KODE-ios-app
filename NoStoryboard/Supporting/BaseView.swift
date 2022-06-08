c5eb00e6ce8a362a00dc054e97be5077e4373531
kirill-kovalev <kirilkovalev@ya.ru>
2022-06-08 18:44:33 +0300

    написал класс базовой вьюшки, чтоб потом в каждой следующей не надо было писать конструкторы и прочий лишний код

---
+++ NoStoryboard/Supporting/BaseView.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+class BaseView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setup() // этва штука вызывается если класс создан не из кода, а через сториборд
+    }
+    
+    func setup() {
+        // пустой метод, который я буду оверрайдить
+    }
+}

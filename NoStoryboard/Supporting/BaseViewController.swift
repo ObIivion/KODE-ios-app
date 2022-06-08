e3a47eb0abd0f426849de708da124a5bfea5b4d6
kirill-kovalev <kirilkovalev@ya.ru>
2022-06-08 18:47:06 +0300

    покольку у нас может быть много контроллеров, чтоб не писать каждый раз создание вьюшки я написал один шаблонный класс - он сам будет создавать вьюшку из нужного классаа когда вызывается loadView. и сразу же в нем cделал mainView - чтоб тоже не писать в каждом контроллере

---
+++ NoStoryboard/Supporting/BaseViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class BaseViewController<T: UIView>: UIViewController {
+    
+    var mainView: T { view as! T }
+    
+    override func loadView() {
+        view = T()
+    }
+}

ca7ac13db25edf64240204bec3c5f1be58b9a289
Pavel <60381965+ObIivion@users.noreply.github.com>
2022-06-30 19:00:32 +0300

    add Lost Internet View

---
+++ NoStoryboard/MainScreen/Views/LostInternetConnectionView.swift
@@ -0,0 +1,102 @@
+//
+//  NoInternetConnectionView.swift
+//  NoStoryboard
+//
+//  Created by Павел Виноградов on 20.06.2022.
+//
+
+import UIKit
+import Rswift
+
+class LostInternetConnectionView: BaseView {
+    
+    private let NLOImageView: UIImageView = {
+        
+        let imageView = UIImageView()
+        imageView.image = R.image.nlO()
+        imageView.clipsToBounds = true
+        imageView.layer.borderWidth = 0
+        return imageView
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Какой-то сверхразум всё сломал"
+        label.numberOfLines = 0
+        label.font = UIFont(name: "Gill Sans SemiBold" , size: 17)
+        label.textColor = UIColor(red: 0.05, green: 0.05, blue: 0.16, alpha: 1)
+        return label
+    }()
+    
+    private let subTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Постараемся быстро починить"
+        label.numberOfLines = 0
+        label.font = UIFont(name: "Gill Sans Regular" , size: 16)
+        label.textColor = UIColor(red: 151/255, green: 151/255, blue: 155/255, alpha: 1)
+        return label
+    }()
+    
+    let tryAgainButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .clear
+        button.setTitle("Попробовать снова", for: .normal)
+        button.setTitleColor(UIColor(red: 101/255, green: 52/255, blue: 1, alpha: 1), for: .normal)
+        button.setTitle("Меня нажали", for: .highlighted)
+        button.titleLabel?.font = UIFont(name: "Gill Sans SemiBold" , size: 16)
+        return button
+    }()
+
+    override func setup() {
+        
+        addSubview(NLOImageView)
+        addSubview(titleLabel)
+        addSubview(subTitleLabel)
+        addSubview(tryAgainButton)
+        
+        setupNLOImageViewConstraints()
+        setupTitleLabelConstraints()
+        setupSubTitleLabelConstraints()
+        setupTryAgainButtonConstraints()
+        
+    }
+    
+    func setupNLOImageViewConstraints(){
+        
+        NLOImageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            NLOImageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            NLOImageView.centerYAnchor.constraint(equalTo: centerYAnchor, constant: -50)
+        ])
+        
+    }
+    
+    func setupTitleLabelConstraints(){
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            titleLabel.topAnchor.constraint(equalTo: NLOImageView.bottomAnchor, constant: 8)
+        ])
+    }
+    
+    func setupSubTitleLabelConstraints(){
+        
+        subTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            subTitleLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            subTitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 12)
+        ])
+    }
+    
+    func setupTryAgainButtonConstraints(){
+        
+        tryAgainButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tryAgainButton.centerXAnchor.constraint(equalTo: centerXAnchor),
+            tryAgainButton.topAnchor.constraint(equalTo: subTitleLabel.bottomAnchor, constant: 12)
+        ])
+        
+    }
+
+}
